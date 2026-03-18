3fca0113060d47fd55adebc99693f44db436000f
wbjackson <126893412+wbjackson@users.noreply.github.com>
2026-03-18 00:36:03 -0400

    Add files via upload

--- src/main/java/SITS_sprint1/RoundRobinTournament.java
+++ src/main/java/SITS_sprint1/RoundRobinTournament.java
@@ -11,42 +11,37 @@ public class RoundRobinTournament extends Tournament
 	}
 
 	@Override
-	public Robot runTournament()
-	{
-		if (participants == null || participants.size()< 2) {
-			String message = "not enough participants";
-			System.out.println(message);
-			return null; //maybe throw exception
-		}else {
-			System.out.println(participants.size());
-		}
-		
-		for (int i = 0; i < participants.size(); i++) {
-			
-			for (int j = i + 1; j < participants.size(); j++) {
-				
-				Robot p1 = participants.get(i);
-				Robot p2 = participants.get(j);
-				
+	public Robot runTournament() {
 
-				game.playGame(p1, p2);
+	    if (participants == null || participants.size() < 2) {
+	        throw new IllegalArgumentException("Not enough participants");
+	    }
 
-				p1.addRecord(p1.getScore());
-				p2.addRecord(p2.getScore());
-			}
-		}
-	
-	
-		
-		Robot best = participants.get(0);
-		
-		for (Robot r : participants) {
-			if (r.record > best.record) {
-				best = r;
-			}else if (r.record == best.record && r.score > best.score) { 
-				best = r;
-			}
-		}
-		return best;
+	    System.out.println(participants.size());
+
+	    for (int i = 0; i < participants.size(); i++) {
+	        for (int j = i + 1; j < participants.size(); j++) {
+
+	            Robot p1 = participants.get(i);
+	            Robot p2 = participants.get(j);
+
+	            game.playGame(p1, p2);
+
+	            p1.addRecord(p1.getScore());
+	            p2.addRecord(p2.getScore());
+	        }
+	    }
+
+	    Robot best = participants.get(0);
+
+	    for (Robot r : participants) {
+	        if (r.record > best.record) {
+	            best = r;
+	        } else if (r.record == best.record && r.score > best.score) {
+	            best = r;
+	        }
+	    }
+
+	    return best;
 	}
 }
